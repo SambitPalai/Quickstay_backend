fbae0bb999b0d216706ac7d832618c8f447cf42e
Sambit Palai <158765214+SambitPalai@users.noreply.github.com>
2026-03-30 15:06:38 +0530

    Add Optional import to RoomRepository

--- src/main/java/com/hotel/springbackend/repository/RoomRepository.java
+++ src/main/java/com/hotel/springbackend/repository/RoomRepository.java
@@ -1,7 +1,8 @@
 package com.hotel.springbackend.repository;
 
 import java.util.List;
-
+import java.util.Optional;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
