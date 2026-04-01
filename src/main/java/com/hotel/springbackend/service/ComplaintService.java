3ee3842325b1f6eb32d070992b6cd1216ce992c7
Sambit Palai <158765214+SambitPalai@users.noreply.github.com>
2026-04-01 18:59:48 +0530

    Add transactional support to ComplaintService

--- src/main/java/com/hotel/springbackend/service/ComplaintService.java
+++ src/main/java/com/hotel/springbackend/service/ComplaintService.java
@@ -11,6 +11,7 @@ import com.hotel.springbackend.request.ComplaintRequest;
 import com.hotel.springbackend.response.ComplaintResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
