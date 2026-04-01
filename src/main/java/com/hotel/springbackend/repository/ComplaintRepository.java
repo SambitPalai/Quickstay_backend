210a3a8198561ae5442cde710961be435031b3ee
Sambit Palai <158765214+SambitPalai@users.noreply.github.com>
2026-04-01 19:00:27 +0530

    Add imports for Query and Param in ComplaintRepository

--- src/main/java/com/hotel/springbackend/repository/ComplaintRepository.java
+++ src/main/java/com/hotel/springbackend/repository/ComplaintRepository.java
@@ -2,7 +2,10 @@ package com.hotel.springbackend.repository;
 
 import com.hotel.springbackend.model.Complaint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.List;
 
 public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
